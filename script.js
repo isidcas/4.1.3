723dc09d2b51807a910009d0100b5adb684f532d
isma <isma@isma>
2025-04-02 18:54:52 +0200

    ejercicio2

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ let randomNumber1 = Math.round(Math.random() * 100);
 console.log(randomNumber1);
 
 //Muestra por consola el valor de PI.
+
+console.log(Math.PI);
+
 //Genera un número aleatorio entre 5 y 10 redondeando y muéstralo por consola. Para  este caso lo recomendado es utilizar la fórmula (Math.random()* (max-min)+min).
 //Comprueba el funcionamiento del método sign(). 
