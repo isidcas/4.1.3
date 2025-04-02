6c7740eead105a47587dc556afa0d80ec9302656
isma <isma@isma>
2025-04-02 18:55:23 +0200

    ejercicio3

--- script.js
+++ script.js
@@ -8,4 +8,10 @@ console.log(randomNumber1);
 console.log(Math.PI);
 
 //Genera un número aleatorio entre 5 y 10 redondeando y muéstralo por consola. Para  este caso lo recomendado es utilizar la fórmula (Math.random()* (max-min)+min).
+
+let min = 5;
+let max = 10;
+let randomNumber2 = Math.round(Math.random() * (max - min) + min);
+console.log(randomNumber2);
+
 //Comprueba el funcionamiento del método sign(). 
