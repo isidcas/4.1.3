7d75073c06890e8903fee16404f660b20185219b
isma <isma@isma>
2025-04-02 18:53:54 +0200

    creando index.html y linkeandolos

---
+++ script.js
@@ -0,0 +1,4 @@
+//Genera un número aleatorio entre 0 y 100 redondeando y muéstralo por consola
+//Muestra por consola el valor de PI.
+//Genera un número aleatorio entre 5 y 10 redondeando y muéstralo por consola. Para  este caso lo recomendado es utilizar la fórmula (Math.random()* (max-min)+min).
+//Comprueba el funcionamiento del método sign(). 
